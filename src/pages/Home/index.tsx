5052d4b2763a45035bf2f576a9dec2316e634d9d
win.xin <it8099@vedeng.com>
2021-02-01 16:45:13 +0800

    JenkinsFile9

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -58,7 +58,6 @@ const Home = () => {
       {
         loadingFlag ? <GobalLoading/> : (
           <HomeWrap>
-            789
             <ArticleList articleList={articleList}/>
             <Pager total={totalNum} pageNum={pageNum} handlePageNum={handlePageNum}/>
           </HomeWrap>
